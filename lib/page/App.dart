343e662d8616197646cf694fdd3ad00bad3a39d1
duke <dukebs1128@gmail.com>
2024-12-24 16:46:21 +0900

    feat : 당근마켓 클론

---
+++ lib/page/App.dart
@@ -0,0 +1,87 @@
+import 'package:carrot_market_clone/page/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:intl/intl.dart';
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _App();
+}
+
+class _App extends State<App> {
+  late List<Map<String, String>> datas;
+  late int _currentPageIndex;
+
+  @override
+  void initState() {
+    super.initState();
+    _currentPageIndex = 0;
+  }
+
+  Widget _bodyWidget() {
+    switch (_currentPageIndex) {
+      case 0:
+        return Home();
+      case 1:
+        return Container();
+      case 2:
+        return Container();
+      case 3:
+        return Container();
+      case 4:
+        return Container();
+    }
+    return Container();
+  }
+
+  //botton nav 요소
+  BottomNavigationBarItem _bottomNavigationBarItem(
+      String iconPath, String label) {
+    return BottomNavigationBarItem(
+        icon: Padding(
+          padding: const EdgeInsets.only(bottom: 2.0),
+          child: SvgPicture.asset(
+            'assets/svg/${iconPath}_off.svg',
+            width: 22,
+          ),
+        ),
+        activeIcon: Padding(
+          padding: const EdgeInsets.only(bottom: 2.0),
+          child: SvgPicture.asset(
+            'assets/svg/${iconPath}_on.svg',
+            width: 22,
+          ),
+        ),
+        label: label);
+  }
+
+  Widget _bottomNavigationBarWidget() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      onTap: (int index) => {
+        setState(() {
+          _currentPageIndex = index;
+        })
+      },
+      selectedFontSize: 12,
+      currentIndex: _currentPageIndex,
+      items: [
+        _bottomNavigationBarItem('home', '홈'),
+        _bottomNavigationBarItem('notes', '동네 생활'),
+        _bottomNavigationBarItem('location', '내 근처'),
+        _bottomNavigationBarItem('chat', '채팅'),
+        _bottomNavigationBarItem('user', '나의 당근'),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _bodyWidget(),
+      bottomNavigationBar: _bottomNavigationBarWidget(),
+    );
+  }
+}
---
+++ lib/page/Home.dart
@@ -0,0 +1,210 @@
+import 'package:carrot_market_clone/repository/content_repository.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:intl/intl.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _Home();
+}
+
+class _Home extends State<Home> {
+  late String _currentLocation;
+  late ContentRepository contentRepository;
+  final Map<String, String> locationTypeToString = {
+    "banyeo": "반여동",
+    "taepyeong": "태평동",
+    "bokjung": "복정동"
+  };
+
+  @override
+  void initState() {
+    super.initState();
+
+    _currentLocation = 'banyeo';
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    contentRepository = ContentRepository();
+  }
+
+  //appBar 위젯
+  PreferredSizeWidget _appbarWidget() {
+    return AppBar(
+      title: GestureDetector(
+        onTap: () => {print('클릭')},
+        child: PopupMenuButton<String>(
+          offset: Offset(0, 35),
+          shape: ShapeBorder.lerp(
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              1),
+          onSelected: (value) => {
+            setState(() {
+              _currentLocation = value;
+            })
+          },
+          itemBuilder: (BuildContext context) {
+            return [
+              const PopupMenuItem(value: 'banyeo', child: Text('반여동')),
+              const PopupMenuItem(value: 'taepyeong', child: Text('태평동')),
+              const PopupMenuItem(value: 'bokjung', child: Text('복정동')),
+            ];
+          },
+          child: Row(
+            children: [
+              Text(
+                locationTypeToString[_currentLocation]!,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const Icon(Icons.arrow_drop_down),
+            ],
+          ),
+        ),
+      ),
+      shape: Border(
+          bottom: BorderSide(
+        color: Colors.grey[300]!,
+        width: 1,
+      )),
+      actions: [
+        IconButton(onPressed: () => {}, icon: Icon(Icons.search)),
+        IconButton(onPressed: () => {}, icon: Icon(Icons.menu)),
+        IconButton(
+          onPressed: () => {},
+          icon: SvgPicture.asset(
+            "assets/svg/bell.svg",
+            width: 22,
+          ),
+        )
+      ],
+    );
+  }
+
+  //화폐단위 포맷 변경
+  String moneyFormat(String data) {
+    if (data == "무료나눔") {
+      return data;
+    }
+    late String formatData;
+    formatData = NumberFormat('###,###,###,###').format(int.parse(data));
+
+    return '${formatData}원';
+  }
+
+  Future<List<Map<String, String>>> _loadContents() {
+    return contentRepository.getContentsFromLoaction(_currentLocation);
+  }
+
+  _makeDataList(List<Map<String, String>> datas) {
+    return ListView.separated(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      itemBuilder: (BuildContext _context, int index) {
+        return Container(
+          padding: const EdgeInsets.symmetric(vertical: 10),
+          child: Row(
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                child: Image.asset(
+                  datas[index]["image"]!,
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        datas[index]["title"]!,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(fontSize: 15),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        datas[index]["location"]!,
+                        style: const TextStyle(
+                            fontSize: 12,
+                            color: Color.fromARGB(255, 151, 151, 151)),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        moneyFormat(datas[index]["price"]!),
+                        style: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          SvgPicture.asset(
+                            'assets/svg/heart_off.svg',
+                            width: 12,
+                            color: Color.fromARGB(255, 116, 116, 116),
+                          ),
+                          const SizedBox(
+                            width: 5,
+                          ),
+                          Text(datas[index]["likes"]!),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext _context, int index) {
+        return Container(
+          height: 1,
+          color: const Color(0xffd3d3d3),
+        );
+      },
+      itemCount: datas.length,
+    );
+  }
+
+  Widget _bodyWidget() {
+    //비동기
+    return FutureBuilder(
+      future: _loadContents(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Center(
+            child: Text('오류 발생 : ${snapshot.error}'),
+          );
+        }
+        if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+          return _makeDataList(snapshot.data!);
+        }
+        return const Center(
+          child: Text("데이터 없음"),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _appbarWidget(),
+      body: _bodyWidget(),
+    );
+  }
+}
